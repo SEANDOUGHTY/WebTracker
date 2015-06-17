c531451dde3faf9f87e22887f6bfbf32f8ae5768
SEANDOUGHTY <sean@taglab.ca>
2015-06-17 17:20:00 -0400

    Adding Map.py

---
+++ Map.py
@@ -0,0 +1,35 @@
+import MySQLdb, urllib, json, time
+
+def connect_database(host, user, password, databasename):
+    db = MySQLdb.connect(host = host,
+                         user = user,
+                         passwd = password,
+                         db = databasename)
+    
+    return db
+
+
+def set_table(database, table):
+    cur = database.cursor()
+    cur.execute("SELECT * FROM " + table)   
+    return cur
+
+
+def parseCars(url):
+    roster = urllib.urlopen(url)    #fetch from url
+    roster = json.load(roster)      #parse into JSON
+    
+    roster = [i for i in roster if i["class_id"] == 3] #only want challengers
+
+    return roster
+
+
+
+
+if __name__ == "__main__":
+    
+    db = connect_database('localhost', 'root', '', 'wordpress2')
+    
+    cur = set_table(db, 'wp_postmeta')
+    
+    
